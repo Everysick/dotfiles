e32ecff324c4f782eaf2b6a7091e6d721f371163
everysick <s.wakeup31@gmail.com>
2016-10-20 09:55:43 +0900

    Update repository

--- package-install.el
+++ package-install.el
@@ -1,6 +1,8 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
+(package-refresh-contents)
 
 (defvar bundle-package-list
   '(
