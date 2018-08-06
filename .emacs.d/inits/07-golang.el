409cc8fddb7f2784486cdcaba2a9bef4cbd650e6
everysick <s.wakeup31@gmail.com>
2018-08-06 11:48:56 +0900

    Add pop-jump-tag in go-mode

--- .emacs.d/inits/07-golang.el
+++ .emacs.d/inits/07-golang.el
@@ -8,8 +8,7 @@
   (setq c-basic-offset 4)
   (setq indent-tabs-mode t)
   (local-set-key (kbd "M-.") 'godef-jump)
-  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-  (local-set-key (kbd "C-c i") 'go-goto-imports)
+  (local-set-key (kbd "M-*") 'pop-tag-mark)
   (local-set-key (kbd "C-c d") 'godoc)
   (add-hook 'before-save-hook 'gofmt-before-save))
 
